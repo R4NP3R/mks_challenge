8bbfaeb60890d4e298d26469466ba10072b3414f
R4NP3R <ssrafa.2014@gmail.com>
2024-05-03 20:51:05 -0300

    arrumando erro no SkeletonLoading

--- src/components/SkeletonLoading/index.tsx
+++ src/components/SkeletonLoading/index.tsx
@@ -4,7 +4,7 @@ export const LoadingSkeleton = () => {
   return (
   <S.LoadingSection data-testid='loading'>
     {Array.from({length: 8}).map((_, i) => (
-      <S.LoadingContainer
+      <S.LoadingContainer key={i}
       animate={{backgroundImage: 'linear-gradient(#f1f1f1,#b1b1b1)'}}
       transition={{duration: 2, repeat: Infinity, ease:'linear'}}
       />
